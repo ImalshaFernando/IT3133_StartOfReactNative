571c80fba63cd9f8d98c054e9fb33580a059092d
Imalsha Fernando <139493584+ImalshaFernando@users.noreply.github.com>
2024-12-21 06:37:16 +0000

    Index page is edited

---
+++ reactnativeapp/app/(tabs)/index.tsx
@@ -0,0 +1,33 @@
+import { Image,  StyleSheet,  Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image source={require("../assets/images/react-logo.png")} style={styles.logo}/>
+      <Text style={styles.textstyle}>Hello World!!</Text>
+      <Text style={styles.subtext}>My First React Native Coss Platform App</Text>
+    </View>
+  );
+}
+
+const styles= StyleSheet.create({
+  logo:{
+    width:200,
+    height:200,
+    borderWidth:2,
+    borderStyle:"dotted"
+  },
+  textstyle:{
+    fontFamily:"Times New Roman",
+    fontSize: 45,
+  },
+  subtext:{
+    fontWeight:500
+  }
+})
